f67a61a9bbd406c6f8b07ae5ba40e3dbd4191b06
Sam Poate <sampoate@outlook.com>
2019-02-01 16:41:53 +0000

    Gallery Page Holder

--- js/main.js
+++ js/main.js
@@ -22,3 +22,18 @@ $('#navbar a').on('click', function(event) {
         );
     }
 });
+
+$('.btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 65
+            },
+            800
+        );
+    }
+});
