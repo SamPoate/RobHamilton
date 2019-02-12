13f481f8088a8ad82eb755961ce0fd3c77893a41
Sam Poate <sampoate@outlook.com>
2019-02-12 12:55:13 +0000

    JS and navbar update

--- js/main-atlantic-titan.js
+++ js/main-atlantic-titan.js
@@ -1,39 +1,39 @@
 //Sticky menu background
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.background = rgba(243, 243, 243, 1);
-    } else {
-        document.querySelector('#navbar').style.background = rgba(243, 243, 243, 0);
-    }
-});
+$(document).on('scroll', function (e) {
+
+    var alpha = $(document).scrollTop() / 250;
+
+           $('#navbar').css('background-color', 'rgba(243,243,243,' + alpha + ')');
+           $('#navbar ul a').css('color', '#333');
+ });
 
 //Smooth Srolling
-$('#navbar a').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+$("#navbar a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 65
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      800
+    );
+  }
 });
 
-$('.btn').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+$(".btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 65
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      800
+    );
+  }
 });
